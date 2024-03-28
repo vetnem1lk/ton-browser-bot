8fad77eb7a607debc1bd10855910cbd52e047c02
vetnem1lk <vetnem1lk@mail.ru>
2024-03-28 07:29:30 +0700

    f comm vc

---
+++ frontend/main.js
@@ -0,0 +1,8 @@
+// Получаем ссылку на форму по её id
+const form = document.getElementById('searchForm');
+
+// Обработчик события отправки формы
+form.addEventListener('submit', async (event) => {
+    // Предотвращаем стандартное поведение формы (перезагрузку страницы)
+
+});
